9ef9f16326435831c98c97858604b0a7caad3e88
jiangyi-creator <1432866213@qq.com>
2026-06-02 14:22:34 +0800

    03axios基础封装

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { getCategoryAPI } from '@/apis/test.js'
+getCategoryAPI().then(res => {
+  console.log(res)
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
