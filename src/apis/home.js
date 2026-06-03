d741eaee5404818878ba959a829a4b3290449585
jiangyi-creator <1432866213@qq.com>
2026-06-03 11:29:46 +0800

    home-新鲜好物业务实现(Panel组件封装)

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,19 @@
+// 获取banner轮播图
 import  httpInstance  from '@/utils/http'
 
 export function getBannerAPI() {
   return httpInstance({
     url: '/home/banner'
+  })
+}
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url:'/home/new'
   })
 }
