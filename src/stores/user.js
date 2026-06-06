2c0d362eff9ff13afbbfbb5a2af6c51c8158f4bf
jiangyi-creator <1432866213@qq.com>
2026-06-06 16:12:19 +0800

    登录-退出登录业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,10 +11,17 @@ export const useUserStore = defineStore('user', () => {
     const res = await loginAPI({account, password})
     userInfo.value = res.result
   }
+
+  // 4.退出登录清除用户信息
+  const clearUserInfo = () => {
+    userInfo.value = {}
+  }
+
   // 3.以对象形式return 出去
   return {
     userInfo,
-    getUserInfo
+    getUserInfo,
+    clearUserInfo
   }
 }, {
   persist: true
--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,6 +1,17 @@
 <script setup>
 import { useUserStore } from '@/stores/user';
+import { useRouter } from 'vue-router';
+const router = useRouter()
 const userStore = useUserStore()
+const confirm = () => {
+  // console.log('退出')
+  // 退出登录功能实现
+  // 1.清除用户信息
+  userStore.clearUserInfo()
+  // 2.跳转到登录页面
+  router.push('/login')
+}
+
 </script>
 
 <template>
@@ -10,7 +21,7 @@ const userStore = useUserStore()
         <template v-if="userStore.userInfo.token">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
