04a64d80c073c66dbeb13d671201b36030c375b9
jiangyi-creator <1432866213@qq.com>
2026-06-06 19:22:28 +0800

    本地-头部购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -21,9 +21,15 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
+  // 删除购物车功能
+  const delCart = (skuId) => {
+    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+    cartList.value.splice(idx, 1)
+  }
   return {
     cartList,
-    addCart
+    addCart,
+    delCart
   }
 }, {
   persist: true
