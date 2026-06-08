1de54e66936fde3b68b7fe64e299ac7fcbd80a13
jiangyi-creator <1432866213@qq.com>
2026-06-08 16:11:32 +0800

    会员中心-个人信息渲染

--- src/apis/user.js
+++ src/apis/user.js
@@ -12,3 +12,13 @@ export function loginAPI({account, password}) {
     }
   })
 }
+
+// 会员中心中猜你喜欢接口
+export const getLikeListAPI = ({ limit = 4 }) => {
+  return request({
+    url:'/goods/relevant',
+    params: {
+      limit
+    }
+  })
+}
