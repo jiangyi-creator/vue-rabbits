9e1548eabf191d6c0abbdb803564a447e6947d60
jiangyi-creator <1432866213@qq.com>
2026-06-12 12:38:25 +0800

    修复API：http 改为 https 解决混合内容拦截

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -19,8 +19,8 @@ onMounted(() => getOrder())
 // 跳转支付
 // 携带订单id以及回调地址跳转到支付地址
 // 支付地址
-const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
-const backURL = 'http://127.0.0.1:5173/paycallback'
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'https://127.0.0.1:5173/paycallback'
 const redirectUrl = encodeURIComponent(backURL)
 const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
